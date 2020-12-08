453d47e4b90e16d75351fde176023247050df3b0
Javier Urzua Ahumada <48871135+Lineamingu@users.noreply.github.com>
2020-12-08 20:50:25 -0300

    Add files via upload

--- start.py
+++ start.py
@@ -5,13 +5,13 @@ import socket
 import sqlite3
 import time
 
-def iniciar(m=0,s=0):
+
+def iniciar(m=0, s=0):
     global tiempo_activo
     global tiempo_descanso
     global tiempo_total
     global count
     global sock
-
 
     while True:
         time.sleep(1)
@@ -19,7 +19,10 @@ def iniciar(m=0,s=0):
         sock.send("00010start"+str(s))
         print(s)
 
-    
+        if s >= 60:
+            s = 0
+            m = m+1
+
         if (m == tiempo_total):
             sock.send("00020startFIN")
             break
@@ -30,16 +33,19 @@ def iniciar(m=0,s=0):
             data = "000"+str(aux)+"start"+data
             sock.send(data.encode())
 
-        if (s == tiempo_descanso):
+        elif (s == tiempo_descanso):
             data = "EJERCICIO "+str(count+1)+": "+lista_ejercicios[count]
             aux = len(data)
             data = "000"+str(aux)+"start"+data
             sock.send(data.encode())
-            count+=1
+            count += 1
 
-        if s >= 60:
-            s=0
-            m=m+1
+        if count == 0:
+            data = "EJERCICIO "+str(count+1)+": "+lista_ejercicios[count]
+            aux = len(data)
+            data = "000"+str(aux)+"start"+data
+            sock.send(data.encode())
+            count += 1
 
         """if(s + 10 == tiempo_activo):
             data = "FALTAN 10 SEGUNDOS PARA EL DESCANSO"
@@ -55,13 +61,10 @@ def iniciar(m=0,s=0):
             sock.send(data.encode())"""
 
 
-
-
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host ="200.14.84.235"
-port =5000
-sock.connect((host,port))
+host = "200.14.84.235"
+port = 5000
+sock.connect((host, port))
 
 sock.send('02000sinitstart'.encode())
 data = sock.recv(2010).decode()
@@ -71,7 +74,7 @@ if(data):
 conn = sqlite3.connect('mrmuscle.sqlite')
 cursor = conn.cursor()
 
-count = 1
+count = 0
 
 while True:
 
@@ -79,36 +82,32 @@ while True:
     if(id):
         id = id[10:]
         lista_ejercicios = []
-        cursor.execute('SELECT Exercise.name FROM Exercise, Routine_exercise, Routine WHERE Exercise.id = Routine_exercise.id_ex AND Routine_exercise.id_routine = Routine.id AND Routine.id = ?',(id,))
+        cursor.execute('SELECT Exercise.name FROM Exercise, Routine_exercise, Routine WHERE Exercise.id = Routine_exercise.id_ex AND Routine_exercise.id_routine = Routine.id AND Routine.id = ?', (id,))
         rows = cursor.fetchall()
         print(rows)
         for row in rows:
             lista_ejercicios.append(row[0])
 
-
+        print("LARGO DE LISTA: ", len(lista_ejercicios))
         lista_detalle = []
-        cursor.execute('SELECT Exercise.detail FROM Exercise, Routine_exercise, Routine WHERE Routine_exercise.id_ex = Exercise.id AND Routine_exercise.id_routine = Routine.id AND Routine.id = ?',(id,))
+        cursor.execute('SELECT Exercise.detail FROM Exercise, Routine_exercise, Routine WHERE Routine_exercise.id_ex = Exercise.id AND Routine_exercise.id_routine = Routine.id AND Routine.id = ?', (id,))
         rows = cursor.fetchall()
         print(rows)
         for row in rows:
             lista_detalle.append(row[0])
 
-        cursor.execute('SELECT active_time FROM Routine WHERE Routine.id = '+str(id))
+        cursor.execute(
+            'SELECT active_time FROM Routine WHERE Routine.id = '+str(id))
         active_time = cursor.fetchall()
         tiempo_activo = active_time[0][0]
 
         tiempo_descanso = 60
 
-        cursor.execute('SELECT total_time FROM Routine WHERE Routine.id = '+str(id))
+        cursor.execute(
+            'SELECT total_time FROM Routine WHERE Routine.id = '+str(id))
         total_time = cursor.fetchall()
         tiempo_total = total_time[0][0]
         print(total_time)
 
-
-
-
-        data = "EJERCICO 1 "+str(lista_ejercicios[0])
-        aux = len(data)
-        data = "000"+str(aux)+"start"+data
-        sock.send(data.encode())
         iniciar()
+        count = 0
