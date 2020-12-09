d922ae471f1ef94fb38bf920116b293e4be58a3e
dEmanu12 <48997671+dEmanu12@users.noreply.github.com>
2020-12-09 00:41:28 -0400

    Update start.py

--- start.py
+++ start.py
@@ -15,53 +15,35 @@ def iniciar(m=0, s=0):
     global largo
 
     while True:
+
         time.sleep(1)
         s += 1
-
-        if (s != 60):
-            sock.send("00010start"+str(s))
+        print(s)
 
         if s == 60:
-            s = 0
-            m = m+1
+            m+=1
 
         if (m == tiempo_total):
             sock.send("00020startFIN")
             break
 
         if(s == tiempo_activo):
-            data = "COMENZANDO DESCANSO"
+            data = "COMENZANDO DESCANSO\n"
             aux = len(data)
             data = "000"+str(aux)+"start"+data
             sock.send(data.encode())
 
-        elif (s == tiempo_descanso and count < largo):
-            data = "EJERCICIO "+str(count+1)+": "+lista_ejercicios[count]
+        elif ((s == tiempo_descanso and count < largo) or count == 0)  :
+            data = "EJERCICIO "+str(count+1)+": "+lista_ejercicios[count]+"\nDETALLE " + lista_detalle[count]+"\n"
             aux = len(data)
-            data = "000"+str(aux)+"start"+data
-            sock.send(data.encode())
-            count += 1
-
-        if count == 0:
-            data = "EJERCICIO "+str(count+1)+": "+lista_ejercicios[count]
-            aux = len(data)
-            data = "000"+str(aux)+"start"+data
+            data = "02000start"+data
             sock.send(data.encode())
             count += 1
 
-        """if(s + 10 == tiempo_activo):
-            data = "FALTAN 10 SEGUNDOS PARA EL DESCANSO"
-            aux = len(data)
-            data = "000"+str(aux)+"start"+data
-            sock.send(data.encode())
+        sock.send("00010start"+str(s))
 
-        elif(s + 10 == 60):
-
-            data = "10 SEGUNDOS PARA EL SIGUIENTE EJERCICIO"
-            aux = len(data)
-            data = "000"+str(aux)+"start"+data
-            sock.send(data.encode())"""
-
+        if s == 60:
+            s = 0    
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "200.14.84.235"
@@ -80,11 +62,11 @@ count = 0
 id = 2
 while True:
 
-    #id = sock.recv(2010).decode()
+    id = sock.recv(2010).decode()
 
-    data = sock.recv(2010).decode()
-    if(data):
-        #id = id[10:]
+    
+    if(id):
+        id = id[10:]
         lista_ejercicios = []
         cursor.execute('SELECT Exercise.name FROM Exercise, Routine_exercise, Routine WHERE Exercise.id = Routine_exercise.id_ex AND Routine_exercise.id_routine = Routine.id AND Routine.id = ?', (id,))
         rows = cursor.fetchall()
@@ -114,5 +96,4 @@ while True:
         print(total_time)
 
         iniciar()
-        id = 3
         count = 0
