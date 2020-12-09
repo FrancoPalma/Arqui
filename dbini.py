5cd57a95bd74d02dd3901be5d78545dbd5b8653f
dEmanu12 <48997671+dEmanu12@users.noreply.github.com>
2020-12-08 21:47:02 -0400

    Update dbini.py

--- dbini.py
+++ dbini.py
@@ -13,17 +13,17 @@ def DB(conn, cur):
 
     #MUSCULATURA - Piernas
     # nivel 1
-    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Puente","Recuéstate boca arriba con tus rodillas flexionadas. Contrae tus músculos abdominales. Eleva la cadera del suelo hasta que la cadera esté en línea con las rodillas y los hombros", 1, "Piernas", 1)')
+    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Puente","Recuestate boca arriba con tus rodillas flexionadas. Contrae tus musculos abdominales. Eleva la cadera del suelo hasta que la cadera este en linea con las rodillas y los hombros", 1, "Piernas", 1)')
     cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Sentadilla","Flexiona las rodillas y baja el cuerpo manteniendo la verticalidad, luego regresa a la posicion erguida", 1, "Piernas", 1)')
-    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Peso muerto libre","Colócate de pie con las piernas separadas a la anchura de los hombros, flexiona las rodillas manteniendo la espalda totalmente recta. Empieza a descender utilizando las caderas", 1, "Piernas", 1)')
+    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Peso muerto libre","Colocate de pie con las piernas separadas a la anchura de los hombros, flexiona las rodillas manteniendo la espalda totalmente recta. Empieza a descender utilizando las caderas", 1, "Piernas", 1)')
 
     # nivel 2
-    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Lunge","Esté de pie con las manos en puño apoyadas en la cintura y las piernas ligeramente separadas. Luego de un paso hacia delante", 1, "Piernas", 2)')
-    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Sentadilla sumo","Separa las piernas para que los talones estén con una distancia amplia. Baja hasta que las caderas estén en la misma línea, a la misma altura que las rodillas", 1, "Piernas", 2)')
-    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Puente con una pierna","Acuéstese boca arriba con los brazos a los lados, las rodillas dobladas y los pies apoyados en el suelo. Levante una pierna y levante las caderas tan alto como pueda. Baja las caderas, repite y luego cambia de pierna", 1, "Piernas", 2)')
+    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Lunge","este de pie con las manos en puño apoyadas en la cintura y las piernas ligeramente separadas. Luego de un paso hacia delante", 1, "Piernas", 2)')
+    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Sentadilla sumo","Separa las piernas para que los talones esten con una distancia amplia. Baja hasta que las caderas esten en la misma linea, a la misma altura que las rodillas", 1, "Piernas", 2)')
+    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Puente con una pierna","Acuestese boca arriba con los brazos a los lados, las rodillas dobladas y los pies apoyados en el suelo. Levante una pierna y levante las caderas tan alto como pueda. Baja las caderas, repite y luego cambia de pierna", 1, "Piernas", 2)')
 
     # nivel 3
-    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Sentadilla con salto","Esté de pie con los pies juntos, y coloca las manos sobre los muslos. Flexiona las rodillas, salta y separa los pies en pleno salto. Déjate caer con los pies separados a una distancia mayor que los hombros, bajando hasta quedar en posicion de sentadilla", 1, "Piernas", 3)')
+    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Sentadilla con salto","este de pie con los pies juntos, y coloca las manos sobre los muslos. Flexiona las rodillas, salta y separa los pies en pleno salto. Dejate caer con los pies separados a una distancia mayor que los hombros, bajando hasta quedar en posicion de sentadilla", 1, "Piernas", 3)')
     cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Peso muerto con una pierna","Ponte de pie apoyado sobre una sola pierna mientras sujetas dos mancuernas. Flexiona un poco una pierna y levántala detras de ti apoyándote en las caderas. Inclina el torso hacia el suelo a la vez que la pierna trasera queda detras de ti", 1, "Piernas", 3)')
 
     #MUSCULATURA - Torso
@@ -38,7 +38,7 @@ def DB(conn, cur):
 
     # nivel 3
     cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Plancha lateral","En posicion de plancha, con los brazos estirados, gira el tronco de tal manera que el cuerpo quede alineado en una plancha lateral", 1, "Torso", 3)')
-    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Plancha invertida","Sientate en el suelo con las piernas juntas. Recuestate hacia atras formando un angulo de 45º entre el suelo y el tronco, apoyando las manos en el suelo con los brazos perpendiculares al suelo. Levanta la pelvis hacia arriba despegando el cuerpo del suelo y contrayendo los glúteos hasta formar una línea recta con el cuerpo. Debes mantener los talones apoyados en el suelo.", 1, "Torso", 3)')
+    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Plancha invertida","Sientate en el suelo con las piernas juntas. Recuestate hacia atras formando un angulo de 45º entre el suelo y el tronco, apoyando las manos en el suelo con los brazos perpendiculares al suelo. Levanta la pelvis hacia arriba despegando el cuerpo del suelo y contrayendo los glúteos hasta formar una linea recta con el cuerpo. Debes mantener los talones apoyados en el suelo.", 1, "Torso", 3)')
 
     #MUSCULATURA - Abdominales
     # nivel 1
@@ -68,7 +68,7 @@ def DB(conn, cur):
     #CARDIO - Abdominales
     cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Mountain climber","Tumbate boca abajo en el suelo apoyando las manos con los dedos dirigidos hacia el frente a la anchura de los hombros. Luego comienza el movimiento consistente en llevar de manera alterna las rodillas hacia el codo opuesto", 0, "Abdominales", 0)')
     cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Caminar hacia pies","---", 0, "Abdominales", 0)')
-    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Rodillazo al frente","Colócate de pie con un pie situado ligeramente detras de ti. Manten el peso sobre el pie delantero y levanta la rodilla trasera frente a ti mientras llevas el codo contrario hacia esa rodilla. Toca la rodilla con el codo, haz una pausa y repite", 0, "Abdominales", 0)')
+    cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Rodillazo al frente","Colocate de pie con un pie situado ligeramente detras de ti. Manten el peso sobre el pie delantero y levanta la rodilla trasera frente a ti mientras llevas el codo contrario hacia esa rodilla. Toca la rodilla con el codo, haz una pausa y repite", 0, "Abdominales", 0)')
 
     #CARDIO - Otros
     cur.execute('INSERT INTO Exercise (name,detail,type,ex_zone,level) VALUES ("Jumping Jacks","De pie con las piernas juntas y los brazos pegados a los muslos, da un salto manteniendo la espalda recta y las piernas separadas a la anchura de los hombros. Al juntar las piernas, tus brazos han de tocar los muslos", 0, "Otros", 0)')
